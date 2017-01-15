4166a8e1bbb18beb637a3388d7ea35cbcb0b16d8
feelsky <bp.vincentwang@gmail.com>
2017-01-15 09:05:34 +0800

    first update

---
+++ BestNews/src/zttc/itat/controller/HelloController.java
@@ -0,0 +1,17 @@
+package zttc.itat.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	//呵呵
+	@RequestMapping({"/hello","/"})
+	public String hello(String username, Model model){
+		System.out.println(username);
+		model.addAttribute("username",username);
+		return "hello";
+	}
+}
