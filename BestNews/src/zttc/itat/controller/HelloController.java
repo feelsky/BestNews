48d3f5bbdec4b2a5f16f51623ef8bcb8471a4a0c
feelsky <bp.vincentwang@gmail.com>
2017-01-16 21:31:18 +0800

    log4j

--- BestNews/src/zttc/itat/controller/HelloController.java
+++ BestNews/src/zttc/itat/controller/HelloController.java
@@ -1,16 +1,19 @@
 package zttc.itat.controller;
 
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
-	//呵呵
+	private static final Logger logger = Logger.getLogger(HelloController.class);  
+
 	@RequestMapping({"/hello","/"})
 	public String hello(String username, Model model){
 		System.out.println(username);
+		logger.info("This is debug message.");  
 		model.addAttribute("username",username);
 		return "user/hello.ftl";
 	}
