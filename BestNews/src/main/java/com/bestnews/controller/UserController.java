5507841bf5d8db82dfd6ff474fb9d2899b654e15
feelsky <bp.vincentwang@gmail.com>
2017-01-18 21:09:54 +0800

    g1

--- BestNews/src/main/java/com/bestnews/controller/UserController.java
+++ BestNews/src/main/java/com/bestnews/controller/UserController.java
@@ -26,6 +26,7 @@ public class UserController {
 	
 	@RequestMapping(value="/list", method=RequestMethod.GET)
 	public String list(Model model){
+
 		model.addAttribute("users",users);
 		return "user/list.ftl";
 	}
